1e89d82344617a12237aa2d8767f3ca145adf4ea
Yuri Grytsai <yugritsai@gmail.com>
2025-10-19 19:55:29 -0600

    fix: sitee base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yugri.github.io',
-	base: '/my-blog',
+	base: '/',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
