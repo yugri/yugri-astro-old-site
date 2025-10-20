e304fee10164bd1129afde0f7e4c489e012f5603
Yuri Grytsai <yugritsai@gmail.com>
2025-10-19 22:40:21 -0600

    feat: add mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,27 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yugri.github.io',
 	base: '/',
 	output: 'static',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(), sitemap(),
+		mermaid({
+			// Optional: Set a default theme
+			theme: 'forest',
+			// Optional: Enable automatic theme switching based on your site's data-theme
+			autoTheme: true,
+			// Optional: Additional mermaid configuration
+			mermaidConfig: {
+			  flowchart: {
+				curve: 'basis',
+			  },
+			},
+		  }
+		),
+	],
 });
