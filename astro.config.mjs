5c25aabef345a364ff5cdb0ba677e3977f48b292
Yuri Grytsai <yugritsai@gmail.com>
2025-10-19 19:41:33 -0600

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yugri.github.io',
+	base: '/my-blog',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
